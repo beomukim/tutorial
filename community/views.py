06520f493ce94445a64ea8f0602302933e6e00cf
bumwoo <gld55602@gmail.com>
2023-01-23 18:33:43 +0900

    글 생성, 목록, 상세보기 구현

--- community/views.py
+++ community/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
-
+from community.forms import *
 # Create your views here.
+def write(request):
+    if request.method == 'POST':
+        form = Form(request.POST)
+        if form.is_valid:
+            form.save()
+    else:
+        form = Form()
+
+    return render(request, 'write.html', {'form':form})
+
+def list(reqeust):
+    articleList = Article.objects.all()
+    return render(reqeust, 'list.html', {'articleList':articleList})
+
+def view(request, num):
+    article = Article.objects.get(id=num)
+    return render(request, 'view.html', {'article':article})
